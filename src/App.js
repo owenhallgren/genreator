a0b23ecca2117acee082e89767a69162975b1bbb
Owen Hallgren <owenhallgren@gmail.com>
2021-04-25 23:55:00 -0600

    Added router to app.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import './App.css';
 import GenreDisplay from './components/GenreDisplay/GenreDisplay.js'
 import Favorites from './components/Favorites/Favorites.js'
+import Nav from './components/Nav/Nav.js'
+import { Route } from 'react-router-dom';
+
 
 
 class App extends Component { 
@@ -30,7 +33,10 @@ class App extends Component {
   render() {
     return (
       <div className="home-page">
-        <div className="title-container">
+      <Nav />
+      <Route exact path='/' render={() => 
+      <>
+          <div className="title-container">
           <h1 className='title'>Genreator</h1>
         </div>
         <div className="random-container">
@@ -39,9 +45,16 @@ class App extends Component {
         <div className="card-container">
           <GenreDisplay addToFavorites={this.addToFavorites} genre={this.state.randomGenre}/>
         </div>
+        </>
+        }/>
+
+        <Route exact path='/favorites' render={() => 
         <div className="favorites-container">
           <Favorites state={this.state} />
-        </div>        
+        </div>  
+        }/>
+        
+              
       </div>
     )
   }
